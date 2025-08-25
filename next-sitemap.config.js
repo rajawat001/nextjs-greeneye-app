a1c2a87b6acc91c23e167630436bfba4c3c2f84f
himmat <hrajawat1404@gmail.com>
2025-08-25 09:42:03 +0530

    Add extra page on next sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,18 @@ module.exports = {
   siteUrl: 'https://greeneye.foundation',
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  exclude: ['/server-sitemap.xml'],
+  changefreq: 'weekly',
+  priority: 0.7,
+  generateIndexSitemap: true,
+
+  additionalPaths: async (config) => {
+    return [
+      // agar manually add karna hai
+      await config.transform(config, '/blog'),
+      await config.transform(config, '/about'),
+      await config.transform(config, '/volunteer'),
+      // await config.transform(config, '/products'),
+    ];
+  },
 };
